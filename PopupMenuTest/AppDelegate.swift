1d02aebd3176cd737150ab5434d8d50f83cadb04
Jiayi Liu <liujiayi19920724@gmail.com>
2019-02-06 11:48:11 +1300

    init

---
+++ PopupMenuTest/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  PopupMenuTest
+//
+//  Created by Beethoven on 6/02/19.
+//  Copyright © 2019 Jiayi Liu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+
+}
+
---
+++ PopupMenuTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopupMenuTest
+//
+//  Created by Beethoven on 6/02/19.
+//  Copyright © 2019 Jiayi Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
